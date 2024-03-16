d0fb03248ffaeedcf3223d8fefca7f53384864fa
Ankit Gupta (Credera) <ankitg@tadigital.com>
2024-03-16 17:10:40 +0530

    Initial changes and cleaning up boilerplate code

---
+++ src/components/ArticleList/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ArticleList = () => {
+  return (
+    <div className="article-list">
+      <p>Hello World</p>
+    </div>
+  );
+};
+
+export default ArticleList;
